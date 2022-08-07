b33560617ec133da05283f70ae1f8afaa86e259e
malhmana <105228296+malhmana@users.noreply.github.com>
2022-08-07 19:31:57 +0530

    Update otp_extract.py

--- otp_extract.py
+++ otp_extract.py
@@ -6,7 +6,6 @@ def extract_otp(message):
   otp= ""
   for i in range(len(message)):
       prob_otp= name[i : i + 6]
-      print(prob_otp)
       if prob_otp.isdigit() and len(prob_otp) == 6:
           otp+= prob_otp
   return otp
